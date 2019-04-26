9290310e6fa83083951256b01efdf776599928a4
tranminhhien <minhhienit54utc2@gmail.com>
2019-04-26 20:58:22 +0700

    xcvcbcvbn

--- ShopEx01.Web/App_Start/RouteConfig.cs
+++ ShopEx01.Web/App_Start/RouteConfig.cs
@@ -86,7 +86,7 @@ namespace ShopEx01.Web
             routes.MapRoute(
             name: "Default",
             url: "{controller}/{action}/{id}",
-            defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+            defaults: new { controller = "Admin", action = "Index", id = UrlParameter.Optional },
                 namespaces: new string[] { "ShopEx01.Web.Controllers" }
             );
 
